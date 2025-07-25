0f6c32e9ed4b2a5a1ddb020be1c27e2c9985a39b
NikosGour <nikosgournak@gmail.com>
2025-07-25 21:57:38 +0300

    moved receiver logic to own file

---
+++ app/receiver.go
@@ -0,0 +1,50 @@
+package app
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+
+	log "github.com/NikosGour/logging/src"
+)
+
+func receiver(port int) error {
+	address := "0.0.0.0:" + strconv.Itoa(port)
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("On listen: %w", err)
+	}
+	log.Debug("Listening on `%s`", address)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			return fmt.Errorf("On accept: %w", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	var size int64
+	content := bytes.Buffer{}
+
+	err := binary.Read(conn, binary.BigEndian, &size)
+	if err != nil {
+		log.Error("%s", fmt.Errorf("On read size: %w", err))
+		return
+	}
+
+	n, err := io.CopyN(&content, conn, size)
+	log.Debug("n=%v", n)
+	if err != nil {
+		log.Error("%s", fmt.Errorf("On read: %w", err))
+		return
+	}
+
+	log.Debug("Content: `%s`", content.String())
+}
