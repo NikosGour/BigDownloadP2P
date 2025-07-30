43b13d78fb151d330a447c67fe6514365a49e2e0
NikosGour <nikosgournak@gmail.com>
2025-07-30 20:48:59 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -73,35 +73,69 @@ while [ "$1" != "" ]; do
 	shift
 done
 
-if [ "$release_flag" == "true" ]; then
+release_flag_setup() {
 	out_name="$out_name-release"
 	linker_flags="$linker_flags -s -w"
 	tags="-tags= "
 	if [ "$windows_flag" == "true" ]; then
 		linker_flags="$linker_flags -H windowsgui"
 	fi
-fi
+}
 
-# check if parameter one exist and is equal to "windows"
-if [ "$windows_flag" == "true" ]; then
-
-	windows_out_dir="/mnt/c/Users/$windows_user/Desktop/go_projects/$project_name"
-	if [ ! -d "$windows_out_dir" ]; then
-		mkdir -p $windows_out_dir
+add_os_tag() {
+	if [ "$windows_flag" == "true" ]; then
+		if [ "$release_flag" == "true" ]; then
+			tags="-tags=windows_os"
+		else
+			tags="$tags,windows_os"
+		fi
+	else
+		if [ "$release_flag" == "true" ]; then
+			tags="-tags=linux_os"
+		else
+			tags="$tags,linux_os"
+		fi
 	fi
-	out_name="$out_name.exe"
+}
 
+build_out_dir(){
+	out_name=$app_choice'_'$out_name
+	if [ "$windows_flag" == "true" ]; then
+		out_dir="/mnt/c/Users/$windows_user/Desktop/go_projects/$project_name"
+		out_name="$out_name.exe"
+	else
+		out_dir="$project_dir/$out_dir"
+	fi
+}
+
+clean_previous_file() {
 	if [ "$clean_flag" == "true" ]; then
 		log_info "Cleaning previous file"
 		rm $out_dir/$out_name 2>/dev/null
 	fi
+}
 
-	echo "tags: $tags"
-	log_info "Packages to build:"
-	GOOS=windows go list -f '{{.GoFiles}}' $tags ./src/...
+set_env_vars() {
+	export WAYLAND_DISPLAY=""
 
-	log_info "Building for windows"
-	error_output=$(CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -ldflags "$linker_flags" -o $out_dir/$out_name $tags ./src 2>&1)
+	if [ "$windows_flag" == "true" ]; then
+		export CGO_ENABLED=1
+		export CC=x86_64-w64-mingw32-gcc
+		export GOOS=windows
+		export GOARCH=amd64
+	else
+		export GOOS=linux
+		export GOARCH=amd64
+	fi
+}
+
+go_list(){
+	log_info "Files to build:"
+ 	go list -f '{{.GoFiles}}' $tags ./...
+}
+
+go_build(){
+	error_output=$(go build -ldflags "$linker_flags" -o $out_dir/$out_name $tags ./cmd/$app_choice 2>&1)
 
 	if [ $? -ne 0 ]; then
 		log_error "Build failed with error:"
@@ -111,49 +145,57 @@ if [ "$windows_flag" == "true" ]; then
 	else
 		log_success "Build succeeded"
 	fi
+}
 
-	log_info "Moving file to $windows_out_dir"
-	mv $out_dir/$out_name $windows_out_dir
-	if [ "$release_flag" == "false" ]; then
-		printf "$out_name\npause\n" >$windows_out_dir/debug_run.bat
+post_build(){
+	if [ "$windows_flag" == "true" ]; then
+		log_info "Creating debug script for windows"
+		if [ "$release_flag" == "false" ]; then
+			printf "$out_name\npause\n" >$out_dir/debug_run.bat
+		fi
+	else
+		#Linux post build
+		:
 	fi
 
 	log_success "Done Building"
 	echo "---------------------------------"
+}
 
-	if [ "$run_flag" == "true" ]; then
-		log_info "Running the built file"
+run_project() {
+	log_info "Running the built file"
+	if [ "$windows_flag" == "true" ]; then
 		powershell.exe -command "C:/Users/$windows_user/Desktop/go_projects/$project_name/$out_name"
-	fi
-
-else
-	if [ "$clean_flag" == "true" ]; then
-		log_info "Cleaning previous file"
-		rm $project_dir/$out_dir/$out_name #2>/dev/null
-	fi
-
-	echo "tags: $tags"
-
-	log_info "Packages to build:"
-	go list -f '{{.GoFiles}}' $tags $project_dir $project_dir/build
-
-	log_info "Building for linux"
-	error_output=$(go build -ldflags "$linker_flags" -o $project_dir/$out_dir/$app_choice'_'$out_name $tags $project_dir/cmd/$app_choice)
-
-	if [ $? -ne 0 ]; then
-		log_error "Build failed with error:"
-		echo "----------------------------------"
-		echo "$error_output"
-		exit 1
 	else
-		log_success "Build succeeded"
+		$out_dir/$out_name
 	fi
+}
 
-	log_success "Done Building"
-	echo "---------------------------------"
-
-	if [ "$run_flag" == "true" ]; then
-		log_info "Running the built file"
-		$project_dir/$out_dir/$app_choice'_'$out_name
-	fi
+#######################
+if [ "$release_flag" == "true" ]; then
+	release_flag_setup
 fi
+
+add_os_tag
+
+build_out_dir
+
+if [ ! -d "$out_dir" ]; then
+	log_info "Creating output directory: $out_dir"
+	mkdir -p $out_dir
+fi
+
+clean_previous_file
+
+set_env_vars
+
+go_list
+
+go_build
+
+post_build
+
+if [ "$run_flag" == "true" ]; then
+	run_project
+fi
+##################
